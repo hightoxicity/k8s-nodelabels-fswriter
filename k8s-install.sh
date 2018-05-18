13e5a5f8dbba42dcad8f03070366471cbfcee4a9
t.fouchard <t.fouchard@qwant.com>
2018-05-18 17:03:08 +0200

    Proper order

--- k8s-install.sh
+++ k8s-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 kubectl apply -f ./k8s-defs/cluster-role/nodelabels-reader.yaml 
+kubectl apply -f ./k8s-defs/service-account/nodelabels-fswriter.yaml
 kubectl apply -f ./k8s-defs/cluster-role-binding/kube-system-fswriter-nodelabels-reader.yaml
 kubectl apply -f ./k8s-defs/daemon-set/k8s-nodelabels-fswriter.yaml
-kubectl apply -f ./k8s-defs/service-account/nodelabels-fswriter.yaml
